66a770faf75c296be0c84c0a678d98b5fbaf0b0d
Timur Celik <timur.celik@gmx.de>
2020-07-21 15:42:17 +0200

    refactored saveNote in storage

--- src/storage/saveNote.js
+++ src/storage/saveNote.js
@@ -1,15 +1,9 @@
 import AsyncStorage from '@react-native-community/async-storage';
 
-export default async (note) => {
+export default async ({ id, title, notes, reminder }) => {
   try {
-    await AsyncStorage.setItem(
-      note.id,
-      JSON.stringify({
-        title: note.title,
-        notes: note.notes,
-        reminder: note.reminder
-      })
-    );
+    await AsyncStorage.setItem(id, JSON.stringify({ title, notes, reminder }));
+    console.log('SAVE WAS SUCCESSFULL');
   } catch (e) {
     console.log(`Something went wrong: ${e.message}`);
   }
