18e237164130cf443a14fd0114db7d981a124641
Timur Celik <timur.celik@gmx.de>
2020-07-24 12:56:57 +0200

    refactored CreateNoteScreen by creating CreateNoteForm

---
+++ src/components/CreateNoteForm.js
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import moment from 'moment';
+
+const CreateNoteForm = ({ onSaveNote }) => {
+  const [noteTitle, setNoteTitle] = useState('');
+  const [notes, setNotes] = useState('');
+  const [showSetReminderButton, setShowSetReminderButton] = useState(true);
+  const [showReminderButtons, setShowReminderButtons] = useState(false);
+  const [dateTimePickerMode, setDateTimePickerMode] = useState('date');
+  const [show, setShow] = useState(false); //for showing DateTimePicker
+  const [reminderDate, setReminderDate] = useState('');
+  const [reminderTime, setReminderTime] = useState('');
+
+  const generateId = () => {
+    return `_${Math.random().toString(36).substr(2, 9)}`;
+  };
+
+  const getDefaultReminderDate = () => {
+    return moment().add(5, 'days').format('D MMM YYYY');
+  };
+
+  const getDefaultReminderTime = () => {
+    return moment().format('HH:mm');
+  };
+
+  //DateTimePicker Event Handler
+  const onChange = (event, selectedDate) => {
+    setShow(false);
+    switch (dateTimePickerMode) {
+      case 'date':
+        setReminderDate(moment(selectedDate).format('D MMM YYYY'));
+        break;
+      case 'time':
+        setReminderTime(moment(selectedDate).format('HH:mm'));
+        break;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Input label="Title" value={noteTitle} onChangeText={setNoteTitle} />
+      <Input
+        inputStyle={styles.notesInput}
+        multiline
+        placeholder="Write down additional notes"
+        label="Notes"
+        textAlignVertical="top"
+        value={notes}
+        onChangeText={setNotes}
+      />
+      <View style={styles.dateTimeButtonsContainer}>
+        {showSetReminderButton && (
+          <Button
+            title="Set Reminder"
+            onPress={() => {
+              setShowSetReminderButton(false);
+              setShowReminderButtons(true);
+              setReminderDate(getDefaultReminderDate());
+              setReminderTime(getDefaultReminderTime());
+            }}
+          />
+        )}
+        {showReminderButtons && (
+          <>
+            <Button
+              title="Add Date"
+              onPress={() => {
+                setShow(true);
+                setDateTimePickerMode('date');
+              }}
+            />
+            <Button
+              title="Add Time"
+              onPress={() => {
+                setShow(true);
+                setDateTimePickerMode('time');
+              }}
+            />
+          </>
+        )}
+      </View>
+      {showReminderButtons && (
+        <Input
+          label="Reminder"
+          disabled
+          value={reminderDate + ' ' + reminderTime}
+        />
+      )}
+      {show && (
+        <DateTimePicker
+          mode={dateTimePickerMode}
+          value={new Date()}
+          onChange={onChange}
+        />
+      )}
+      <Button
+        title="Save Note"
+        type="outline"
+        onPress={() => {
+          onSaveNote(generateId(), noteTitle, notes, {
+            date: reminderDate,
+            time: reminderTime
+          });
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10
+  },
+  dateTimeButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginBottom: 10
+  },
+  notesInput: {
+    height: 150
+  }
+});
+
+export default CreateNoteForm;
--- src/screens/CreateNoteScreen.js
+++ src/screens/CreateNoteScreen.js
@@ -1,59 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
-import { View, StyleSheet, TextInput } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import moment from 'moment';
+import { StyleSheet } from 'react-native';
 import { addnote } from '../actions/noteActions';
-import DateTimePicker from '@react-native-community/datetimepicker';
 import saveNoteToStorage from '../storage/saveNote';
+import CreateNoteForm from '../components/CreateNoteForm';
 
 const CreateNoteScreen = ({ navigation }) => {
-  const [noteTitle, setNoteTitle] = useState('');
-  const [notes, setNotes] = useState('');
-  const [showSetReminderButton, setShowSetReminderButton] = useState(true);
-  const [showReminderButtons, setShowReminderButtons] = useState(false);
-  const [dateTimePickerMode, setDateTimePickerMode] = useState('date');
-  const [show, setShow] = useState(false); //for showing DateTimePicker
-  const [reminderDate, setReminderDate] = useState('');
-  const [reminderTime, setReminderTime] = useState('');
-
   const dispatch = useDispatch();
   const addNote = (note) => dispatch(addnote(note));
 
-  const generateId = () => {
-    return `_${Math.random().toString(36).substr(2, 9)}`;
-  };
-
-  const getDefaultReminderDate = () => {
-    return moment().add(5, 'days').format('D MMM YYYY');
-  };
-
-  const getDefaultReminderTime = () => {
-    return moment().format('HH:mm');
-  };
-
-  //DateTimePicker Event Handler
-  const onChange = (event, selectedDate) => {
-    setShow(false);
-    switch (dateTimePickerMode) {
-      case 'date':
-        setReminderDate(moment(selectedDate).format('D MMM YYYY'));
-        break;
-      case 'time':
-        setReminderTime(moment(selectedDate).format('HH:mm'));
-        break;
-    }
-  };
-
-  const onSaveNote = async () => {
+  const saveNote = async (id, title, notes, reminder) => {
     const note = {
-      id: generateId(),
-      title: noteTitle,
+      id,
+      title,
       notes,
-      reminder: {
-        date: reminderDate,
-        time: reminderTime
-      }
+      reminder
     };
     try {
       await saveNoteToStorage(note);
@@ -62,80 +23,9 @@ const CreateNoteScreen = ({ navigation }) => {
     } catch (e) {}
   };
 
-  return (
-    <View style={styles.container}>
-      <Input label="Title" value={noteTitle} onChangeText={setNoteTitle} />
-      <Input
-        inputStyle={styles.notesInput}
-        multiline
-        placeholder="Write down additional notes"
-        label="Notes"
-        textAlignVertical="top"
-        value={notes}
-        onChangeText={setNotes}
-      />
-      <View style={styles.dateTimeButtonsContainer}>
-        {showSetReminderButton && (
-          <Button
-            title="Set Reminder"
-            onPress={() => {
-              setShowSetReminderButton(false);
-              setShowReminderButtons(true);
-              setReminderDate(getDefaultReminderDate());
-              setReminderTime(getDefaultReminderTime());
-            }}
-          />
-        )}
-        {showReminderButtons && (
-          <>
-            <Button
-              title="Add Date"
-              onPress={() => {
-                setShow(true);
-                setDateTimePickerMode('date');
-              }}
-            />
-            <Button
-              title="Add Time"
-              onPress={() => {
-                setShow(true);
-                setDateTimePickerMode('time');
-              }}
-            />
-          </>
-        )}
-      </View>
-      {showReminderButtons && (
-        <Input
-          label="Reminder"
-          disabled
-          value={reminderDate + ' ' + reminderTime}
-        />
-      )}
-      {show && (
-        <DateTimePicker
-          mode={dateTimePickerMode}
-          value={new Date()}
-          onChange={onChange}
-        />
-      )}
-      <Button title="Save Note" type="outline" onPress={onSaveNote} />
-    </View>
-  );
+  return <CreateNoteForm onSaveNote={saveNote} />;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    margin: 10
-  },
-  dateTimeButtonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    marginBottom: 10
-  },
-  notesInput: {
-    height: 150
-  }
-});
+const styles = StyleSheet.create({});
 
 export default CreateNoteScreen;
